2c14c85fe5521869d5a9adab7b4426ef39c39e0e
FilipowiczP <przemyslaw.filipowicz1@gmail.com>
2020-09-29 15:25:33 +0200

    Created 3d animation

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
 import './project.scss';
+
+import projects from './projects';
 
 const Project = ({show}) =>{
     return(
         <section className={show === 3 ? "project project-show" : "project"}>
             <h1 className="project__title">Projekty</h1>
             <div className="project__slider">
-
+                {projects.map(({img},index)=>{
+                    return(
+                        <span className={"project__slider__span"+index}>
+                            <img key={index} src={img} alt="project" className="project__slider__img"/>
+                        </span>
+                    ) 
+                })}
             </div>
         </section>
     );
