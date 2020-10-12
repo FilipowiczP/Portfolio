59020fe460158a1c86affa89b5e313d22acbb10e
FilipowiczP <przemyslaw.filipowicz1@gmail.com>
2020-10-12 18:55:13 +0200

    Added emailJs

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,26 +1,40 @@
 import React from 'react';
 import './contact.scss';
 
+import emailjs from 'emailjs-com';
 import PropTypes from 'prop-types';
 
 const Contact = ({show}) =>{
+
+    const sendMail = (e) =>{
+        e.preventDefault();
+
+        emailjs.sendForm('gmail', 'template_yikoxcv', e.target, 'user_N1sIprgV99LBTMUryFsYr')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+          e.target.reset();
+    }
+
     return(
         <section  className={show === 4 ? "contact contact-show" : "contact"}>
             <h1 className="contact__title">Kontatk</h1>
-            <form className="contact__form">
+            <form className="contact__form" onSubmit={e => sendMail(e)}>
                 <label className="contact__form__name">
                     Imię
-                    <input type="name"  className="contact__form__name__input"/>
+                    <input type="name"  className="contact__form__name__input" name="from_name"/>
                 </label>
                 <label className="contact__form__email">
                     Email
-                    <input type="email" className="contact__form__email__input"/>
+                    <input type="email" className="contact__form__email__input" name="from_email"/>
                 </label>
-                <label className="contact__form__topic">
+                <label className="contact__form__subject">
                     Temat
-                    <input type="text" className="contact__form__topic__input"/>
+                    <input type="text" className="contact__form__subject__input" name="subject"/>
                 </label>
-                <textarea className="contact__form__message" placeholder="Wiadomość"/>
+                <textarea className="contact__form__message" placeholder="Wiadomość" name="message"/>
                 <button type="submit" className="contact__form__button">Wyślij</button>
             </form>
         </section>
