e2fb098bdc7f710edfd3044933bd6223dca5d286
FilipowiczP <przemyslaw.filipowicz1@gmail.com>
2020-10-12 17:53:08 +0200

    Created form in contact

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -7,6 +7,18 @@ const Contact = ({show}) =>{
     return(
         <section  className={show === 4 ? "contact contact-show" : "contact"}>
             <h1 className="contact__title">Kontatk</h1>
+            <form className="contact__form">
+                <label className="contact__form__name">
+                    Imię
+                    <input type="text" />
+                </label>
+                <label className="contact__form__email">
+                    Email
+                    <input type="email" />
+                </label>
+                <textarea className="contact__form__message"/>
+                <button type="submit">Wyślij</button>
+            </form>
         </section>
     );
 };
