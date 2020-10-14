bee89e18251240865cf820bcc734555e64fc2295
FilipowiczP <przemyslaw.filipowicz1@gmail.com>
2020-10-14 17:32:42 +0200

    Created validation

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,21 +1,51 @@
-import React from 'react';
+import React, { useState, useReducer } from 'react';
 import './contact.scss';
 
 import emailjs from 'emailjs-com';
 import PropTypes from 'prop-types';
 
+let initialState = {
+    name: "",
+    email: "",
+    subject: "",
+    message: ""
+}
+
+const reducer = (state, action) =>{
+    switch(action.type){
+        case 'name':
+            return state = {...state, name: action.payload};
+        case 'email':
+            return state = {...state, email: action.payload};
+        case 'subject':
+            return state = {...state, subject: action.payload};
+        case 'message':
+            return state = {...state, message: action.payload};
+        default:
+            throw new Error();
+    }
+}
+
 const Contact = ({show}) =>{
+
+    const [send, setSend] = useState (null);
+    const [state, dispatch] = useReducer(reducer, initialState);
+
 
     const sendMail = (e) =>{
         e.preventDefault();
-
-        emailjs.sendForm('gmail', 'template_yikoxcv', e.target, 'user_N1sIprgV99LBTMUryFsYr')
-          .then((result) => {
-              console.log(result.text);
-          }, (error) => {
-              console.log(error.text);
-          });
-          e.target.reset();
+        if(state.name ===  "" || state.email === "" || state.subject === "" || state.message === ""){
+            setSend(false);
+        } else{
+            emailjs.sendForm('gmail', 'template_yikoxcv', e.target, 'user_N1sIprgV99LBTMUryFsYr')
+              .then((result) => {
+                  console.log(result.text);
+              }, (error) => {
+                  console.log(error.text);
+              });
+            setSend(true);
+            e.target.reset();
+        }
     }
 
     return(
@@ -24,17 +54,18 @@ const Contact = ({show}) =>{
             <form className="contact__form" onSubmit={e => sendMail(e)}>
                 <label className="contact__form__name">
                     Imię
-                    <input type="name"  className="contact__form__name__input" name="from_name"/>
+                    <input type="name"  className="contact__form__name__input" name="from_name" onChange={(event) => dispatch({type:'name', payload: event.target.value})}/>
                 </label>
                 <label className="contact__form__email">
                     Email
-                    <input type="email" className="contact__form__email__input" name="from_email"/>
+                    <input type="email" className="contact__form__email__input" name="from_email" onChange={(event) => dispatch({type:'email', payload: event.target.value })}/>
                 </label>
                 <label className="contact__form__subject">
                     Temat
-                    <input type="text" className="contact__form__subject__input" name="subject"/>
+                    <input type="text" className="contact__form__subject__input" name="subject" onChange={(event) => dispatch({type:'subject', payload: event.target.value})}/>
                 </label>
-                <textarea className="contact__form__message" placeholder="Wiadomość" name="message"/>
+                <textarea className="contact__form__message" placeholder="Wiadomość" name="message" onChange={(event) => dispatch({type:'message', payload: event.target.value})}/>
+                {send ? <p style={{color:"green"}}>Wysłano wiadomość</p> : <p style={{color:"red"}}>Uzupełnij wszyskie pola</p>}
                 <button type="submit" className="contact__form__button">Wyślij</button>
             </form>
         </section>
--- src/components/Project/projects.js
+++ src/components/Project/projects.js
@@ -7,27 +7,26 @@ import rumholic from '../../images/rumholic.png';
 const projects =[
     {
         name:"Best Shop",
-        description:"Jest to jeden z pierwszych projektów jaki wykonałem podczas Coders Lab"+
-        " na podstawie dokumentacji napisanej w adobe xd.",
+        description:"Jest to projekt który rozbudowywałem wraz z poznanymi technologiami.",
         technology:"HTML, CSS, JS, Sass, RWD, Gulp",
         img:bestShop,
     },
     {
         name:"Car Sharing",
-        description:"Pierwszy projekt stworzony podczas bootcamp w Coders Lab.",
+        description:"Jeden z moich pierwszych projektów przedstawiający wypożyczalnię aut.",
         technology:"HTML, CSS",
         img:carSharing,
     },
     {
         name:"Give Clothes",
-        description:"Jest to projekt stworzony przes absolewntów 'UX digital designer' z coders lab, Cały projekt był przygotowany na platformie trello oraz adobe xd.",
+        description:"Projekt służący pomocy. Na którym po zalogowaniu wypełniamy formularz w którym możemy wskazać miejsce i komu pomagamy.",
         technology:"React, Sass, RWD, Npm, Firebase",
         img:clothes,
     },
     {
         name:"rumholic.pl",
-        description:"Jest to jedno z ciekawszych zleceń jakie otrzymałem. Jest to blog o ciekawostkach ze światu rumu."+
-        "Klient na chwilę obecną nie posiada wystarczająco treści i część funkcji jest wyłączona.",
+        description:"Jest to blog opisujący świat rumów. Część funkcji jest wyłączona z użytku, ponieważ klient na chwilę " + 
+        "obecną nie posiada wystarczającą ilość treści.",
         technology:"React, Sass, RWD, Npm",
         img:rumholic,
     }
